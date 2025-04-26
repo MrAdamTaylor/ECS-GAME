e6f61d1e188b70f4457603b8ff406a69aa5cda0f
MrAdamTaylor <admataylorgb@gmail.com>
2025-04-26 16:32:23 +0300

    Restruct project

---
+++ src/ECS-GAME/Assets/Code/Gameplay/Common/CommonComponents.cs
@@ -0,0 +1,9 @@
+using Entitas;
+using UnityEngine;
+
+namespace Code.Gameplay.Common
+{
+    [Game] public class Id : IComponent { public int Value; }
+    [Game] public class WorldPosition : IComponent { public Vector3 Value; }
+}
+
