e2b690eef2e3221e1ca16bdca8a779ccf2275470
MrAdamTaylor <admataylorgb@gmail.com>
2025-04-26 21:57:55 +0300

    First ECS System

---
+++ src/ECS-GAME/Assets/Code/Gameplay/Features/Movement/Systems/DirectionalDeltaMoveSystem.cs
@@ -0,0 +1,31 @@
+using Code.Gameplay.Common.Time;
+using Entitas;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Movement.Systems
+{
+    public class DirectionalDeltaMoveSystem : IExecuteSystem
+    {
+        private readonly ITimeService _timeService;
+        private readonly IGroup<GameEntity> _movers;
+
+        public DirectionalDeltaMoveSystem(GameContext gameContext, ITimeService timeService)
+        {
+            _timeService = timeService;
+            _movers = gameContext.GetGroup(GameMatcher.AllOf(
+                GameMatcher.WorldPosition,
+                GameMatcher.Speed,
+                GameMatcher.Direction,
+                GameMatcher.Moving
+            ));
+        }
+
+        public void Execute()
+        {
+            foreach (GameEntity mover in _movers.GetEntities())
+            {
+                mover.ReplaceWorldPosition((Vector2)mover.WorldPosition + mover.Direction * mover.Speed * _timeService.DeltaTime);
+            }
+        }
+    }
+}
